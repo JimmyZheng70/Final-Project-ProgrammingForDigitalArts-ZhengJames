ad47e40c5ebe71dc6a08961e1723c7a9260408b8
James Zheng <jimmyztx@gmail.com>
2026-04-11 15:45:31 -0500

    Create a player variable to get Player class and place the screen into it.

--- src/project.py
+++ src/project.py
@@ -34,11 +34,15 @@ def main():
     fullscreen = (1920, 1080)
     screen = pygame.display.set_mode(smallscreen, pygame.RESIZABLE)
 
+    player = Player(screen)
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        
     pygame.quit()
 
 if __name__ == "__main__":
