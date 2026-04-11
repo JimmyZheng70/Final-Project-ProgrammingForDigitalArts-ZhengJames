8202ac8af152d8a90e7075fe299c54423acc20a5
James Zheng <jimmyztx@gmail.com>
2026-04-11 15:44:34 -0500

    Draw the player, using the resoltuion, size, and the overall rectangular shape.

--- src/project.py
+++ src/project.py
@@ -23,7 +23,7 @@ class Player():
             self.direction = 1
     
     def draw(self):
-        pygame.draw.rect(s)
+        pygame.draw.rect(self.screen, (255, 255, 255), self.shape)
         
 
 def main():
