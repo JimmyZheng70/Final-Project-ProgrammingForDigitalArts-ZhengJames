1e5c5c373974bae853882ef7de5aef3b5ce85f75
James Zheng <jimmyztx@gmail.com>
2026-04-11 15:53:50 -0500

    Cretaed a  self.color and changed with it, and in the future have a way to change the color

--- src/project.py
+++ src/project.py
@@ -2,7 +2,7 @@ import pygame
 # TODO: Create a player rectangle at the bottom, allow player to control that rectangle, finish the resizability of the game, and anything else this week.
 
 class Player():
-    def __init__(self, screen):
+    def __init__(self, screen, color=(0, 0, 255)):
         self.height = 10
         self.width = 100
         self.x = 300
@@ -11,6 +11,7 @@ class Player():
         self.speed = 10
         self.direction = 0
         self.screen = screen
+        self.color = color # Change later to make it change color
     
     def movement(self):
         self.direction = 0
@@ -23,7 +24,7 @@ class Player():
             self.direction = 1
     
     def draw(self):
-        pygame.draw.rect(self.screen, (0, 0, 255), self.shape)
+        pygame.draw.rect(self.screen, self.color, self.shape)
         
 
 def main():
