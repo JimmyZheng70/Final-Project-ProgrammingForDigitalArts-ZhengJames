eba969e225352e639f239524b0f0ad33a8700f99
James Zheng <jimmyztx@gmail.com>
2026-04-16 14:17:41 -0500

    Added a comment for a future fix with resizing screen

--- src/project.py
+++ src/project.py
@@ -1,5 +1,5 @@
 import pygame
-# TODO: Create the Ball and its mechanics, cretae blocks and make them be destroyed, plus anything else if enough time.
+# TODO: Create the Ball and its mechanics, cretae blocks and make them be destroyed, resize the ball and player to match the large screen
 
 class Player():
     def __init__(self, screen, color=(255, 0, 255)):
