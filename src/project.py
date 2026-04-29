c5a527ea561129f14eabe335a9096b282e182e66
James Zheng <jimmyztx@gmail.com>
2026-04-29 14:55:33 -0500

    Adjust size to be smalelr for Pause

--- src/project.py
+++ src/project.py
@@ -239,7 +239,7 @@ def main():
     block = Blocks(screen)
     ball = Ball(350, 630, screen, player, block)
 
-    pause_font = pygame.font.SysFont(None, 250)
+    pause_font = pygame.font.SysFont(None, 150)
     pause_text = pause_font.render("PAUSE", True, (0, 255, 0))
 
     is_fullscreen = False
