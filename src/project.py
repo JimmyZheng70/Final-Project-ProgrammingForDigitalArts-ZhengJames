8a441591f7190a29cf523e9f4a7d0cf1b6561010
James Zheng <jimmyztx@gmail.com>
2026-04-29 12:33:53 -0500

    Attempt to make ball mvoement different when hitting the player through changing x. somewhat different but not great

--- src/project.py
+++ src/project.py
@@ -123,6 +123,7 @@ class Ball():
 
             self.color_switch()
             self.speed_y *= -1
+            self.speed_x += 15
 
         # Checks collision with Blocks
         for block in self.block.block[:]:
